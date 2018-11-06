aafc755ce70a94d7faae217f1f2e29f4f67e2628
valentinalenis <valentinalenis@hotmail.com>
2018-11-05 23:06:02 -0500

    9

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
-import { RadioB } from '../components/RadioB';
+import { View } from 'react-native';
+import { RadioButton } from 'react-native-paper';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
     title: 'app.json',
   };
+  state = {
+    checked: 'first',
+  };
 
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <RadioB/>;
+    const { checked } = this.state;
+    return (
+      <View>
+        <RadioButton
+          value="first"
+          status={checked === 'first' ? 'checked' : 'unchecked'}
+          onPress={() => { this.setState({ checked: 'first' }); }}
+        />
+        <RadioButton
+          value="second"
+          status={checked === 'second' ? 'checked' : 'unchecked'}
+          onPress={() => { this.setState({ checked: 'second' }); }}
+        />
+      </View>
+    );
   }
 }
+
