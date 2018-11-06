4be5a94dd5ff5a33e48c9bdaf588343709c0a7ad
valentinalenis <valentinalenis@hotmail.com>
2018-11-05 22:00:57 -0500

    5

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -84,7 +84,7 @@ export default class HomeScreen extends React.Component {
                 <CardButton
                   onPress={() => {}}
                   title="Like"
-                  color="green"
+                  color="red"
                 />
               </CardAction>
             </Card>
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -7,8 +8,19 @@ export default class SettingsScreen extends React.Component {
   };
 
   render() {
+    var radio_props = [
+      {label: 'param1', value: 0 },
+      {label: 'param2', value: 1 }
+    ];
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return(
+    <RadioForm
+      radio_props={radio_props}
+      initial={0}
+      onPress={(value) => {this.setState({value:value})}}
+    />
+      
+      );
   }
 }
