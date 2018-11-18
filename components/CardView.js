2a626e274d41080bb1052c21a58ed5e6c4c59958
valentinalenis <valentinalenis@hotmail.com>
2018-11-17 21:08:13 -0500

    jsx

--- components/CardView.js
+++ components/CardView.js
@@ -1,5 +1,5 @@
 import React ,{ Component } from 'react';
-import { Card, CardTitle, CardContent, CardAction, CardButton } from 'react-native-material-cards'
+import { Card, CardTitle, CardContent,Text, CardAction, CardButton } from 'react-native-material-cards'
 import axios from "axios";
 import {View} from 'react-native';
 import Event from './Event';
@@ -8,35 +8,40 @@ const API_URL = ':3001';
 
 class cardView extends Component {
     state = {
-      name: 0,
-      place1: 0,
-      place2: 0,
-      place3: 0,
-      date1: 0, 
-      date2: 0,
-      date3: 0,
-      time1: 0, 
-      time2: 0,
-      time3: 0,
-      list: []
-    };
+        name: 0,
+        place1: 0,
+        place2: 0,
+        place3: 0,
+        date1: 0, 
+        date2: 0,
+        date3: 0,
+        time1: 0, 
+        time2: 0,
+        time3: 0,
+        list: []
+        };
     
-    getAllEvents = () => {
+    /*getAllEvents = () => {
+        console.log("Acá estoy");
         axios
         .get("https://shrouded-beyond-36442.herokuapp.com/propuesta")
         .then(res => {
-            const lista = res.map(e => (<Event obj={e}/>))
-            this.setState({list:lista})
-            console.log(res)
+            console.log(res._response);
+
+           res.response.map(e => console.log(e)(<Event obj={e}/>))
+            this.setState({list:lista});
+            console.log("___________________________________");
+            console.log(res);
+            console.log("___________________________________");
         });
         
     }
 
     componentDidMount(){
+        console.log("Por acá también");
         axios
         .get('https://shrouded-beyond-36442.herokuapp.com/evento')
         .then(res => {
-            this.getAllEvents();
             const { data } = res
             this.setState({
                 name: res.name,
@@ -48,25 +53,35 @@ class cardView extends Component {
                 date3: res.date3,
                 time1: res.time1,
                 time2: res.time2,
-                time3: res.time3,
-            
+                time3: res.time3
             });
-            
         })
         .catch((error) =>{
           console.error(error);
         });
-      }
+      }*/
     
       
     render() {
-      
-      return (
-
-        <View>
-            {this.state.list.map(e => e)}
-        </View>
-      )
+        
+        return (
+            <Card>
+                <CardTitle 
+                title="Carrera 10k" 
+                subtitle="Deporte"
+                />
+            <CardContent text="No todo es comida, hay que hacer deporte, gordos." />
+            <CardAction 
+            separator={true} 
+            inColumn={false}>
+            <CardButton
+                onPress={() => {}}
+                title="Like"
+                color="blue"
+            />
+            </CardAction>
+      </Card>
+        )
     }
   
 }
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,58 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
 import StarCal from '../components/Star';
 import axios from "axios";
 
 export default class SettingsScreen extends React.Component {
-  state = {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://shrouded-beyond-36442.herokuapp.com/propuesta')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+          carac: responseJson.characteristics,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.name}, {item.description}, {item.characteristics.place}, {item.characteristics.votes}</Text>}
+          keyExtractor={({_id}, index) => _id}
+        />
+        
+        
+      </View>
+    );
+  }
+  /*state = {
     name: [],
 
   };
@@ -14,7 +62,6 @@ export default class SettingsScreen extends React.Component {
       .get('https://shrouded-beyond-36442.herokuapp.com/evento')
       .then(res => {
           const { data } = res
-          console.log(res)
           this.setState({
           name: res.name,
           place1: res.place1,
@@ -57,7 +104,7 @@ export default class SettingsScreen extends React.Component {
 
     </View>
     );
-  }
+  }*/
 }
 
 const styles = StyleSheet.create({
